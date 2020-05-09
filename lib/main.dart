aff80550ab4b8e0effee1b40fc5576feedf90a46
bindu <bindu@Rajeshs-MacBook-Pro.local>
2020-05-09 15:36:53 +0545

    added details container with icon

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
                   height: 50.0,
                 ),
                 CircleAvatar(
-                  radius: 60.0,
+                  radius: 55.0,
                   backgroundImage: AssetImage('images/profile.jpeg'),
                 ),
                 SizedBox(
@@ -31,7 +31,44 @@ class MyApp extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
                       fontSize: 35.0),
-                )
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  'ANDROID DEVELOPER',
+                  style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.white70,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(5.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.lightBlue,
+                      ),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      Text(
+                        'watsonema@gmail.com',
+                        style:
+                            TextStyle(color: Colors.lightBlue, fontSize: 20.0),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
