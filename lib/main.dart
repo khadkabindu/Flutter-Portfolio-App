d5a356298d9b8bf80ede30bc835f41bf1c717c3c
bindu <bindu@Rajeshs-MacBook-Pro.local>
2020-05-10 16:33:10 +0545

    added firstscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,90 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => FirstScreen(),
+      '/second': (context) => SecondScreen(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 65.0,
+                ),
+                Text(
+                  'Welcome',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontFamily: 'MarckScript',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                Text(
+                  'TO',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontFamily: 'MarckScript',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                Text(
+                  'Make My Portfolio',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontFamily: 'MarckScript',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  child: Image.asset('images/done-5.png'),
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                new RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(25.0)),
+                  splashColor: Colors.yellow,
+                  color: Colors.blue,
+                  elevation: 5.0,
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/second');
+                  },
+                  child: new Text(
+                    "Create now",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,13 +120,16 @@ class MyApp extends StatelessWidget {
                 SizedBox(
                   height: 20.0,
                 ),
-                Text(
-                  'Ema Watson',
-                  style: TextStyle(
-                      fontFamily: 'MarckScript',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 35.0),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    'Ema Watson',
+                    style: TextStyle(
+                        fontFamily: 'MarckScript',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 35.0),
+                  ),
                 ),
                 SizedBox(
                   height: 5.0,
